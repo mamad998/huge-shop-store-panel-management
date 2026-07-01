95bb88eb9b6e8aa41ab6822a9979ad1a5c089266
mohammadreza <mamadm118@gmail.com>
2026-07-01 23:54:14 +0330

    Use environment variable for API URL in fetch

--- apps/admin/app/products/newproduct/page.jsx
+++ apps/admin/app/products/newproduct/page.jsx
@@ -22,7 +22,7 @@ import { useState } from "react"
 
         e.preventDefault();
 
-        const response = await fetch("http://localhost:3001/api/products" , {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products` , {
             method : "POST",
             headers:{"Content-Type" : "application/json"},
             body : JSON.stringify(formData)
