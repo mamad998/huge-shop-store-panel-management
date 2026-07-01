170944db4757281d4dc2846551055f4d51d337dd
mohammadreza <mamadm118@gmail.com>
2026-07-01 23:55:09 +0330

    Update page.jsx

--- apps/admin/app/products/edit/[id]/page.jsx
+++ apps/admin/app/products/edit/[id]/page.jsx
@@ -11,7 +11,7 @@ export default function EditProduct(){
     useEffect(
         ()=>{
             async function fetchProduct(){
-                const response = await fetch(`http://localhost:3001/api/products/${id}`);
+                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products/${id}`);
                 const data = await response.json();
                 setFormData(data.product)
             }
